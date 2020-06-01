b18cd48e480b4ae6aed5c5ded3e49b83444718d8
LucioPg <lucio.di.capua@gmail.com>
2020-06-01 18:19:02 +0200

    initial commit ennesimo

--- RIS/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CategoriaProdotto, Prodotto
-
-admin.site.register(CategoriaProdotto)
-admin.site.register(Prodotto)
-# Register your models here.
--- RIS/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='dashboard'),
-    path('categoria/<int:id>', views.categoria, name='categoria'),
-    path('categorie/', views.categorie, name='categorie'),
-]
